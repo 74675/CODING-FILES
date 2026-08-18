f69e95688b6c7b97fdf2266e682aa61d11c6d612
Harjit singh <hararya218@gmail.com>
2026-08-19 00:25:54 +0530

    first commit

---
+++ python/name and aquared code.py
@@ -0,0 +1,14 @@
+# First Python program
+
+# Print a welcome message
+print("Hello, World!")
+
+# Ask for user input
+name = input("What is your name? ")
+
+# Greet the user
+print("Nice to meet you,", name)
+
+# Do a simple calculation
+number = int(input("Enter a number: "))
+print("Your number squared is:", number ** 2)
